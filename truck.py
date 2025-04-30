3d00ef5dc71ae4d9d9df27cf278615da17ba872e
David Sills <dsills6@wgu.edu>
2025-04-30 18:43:55 -0500

    truck class finished

---
+++ truck.py
@@ -0,0 +1,14 @@
+class truck:
+    def truck(self, speed, capacity, load, packages, mileage, address, departure):
+        self.speed = speed
+        self.capacity = capacity
+        self.load = load
+        self.packages = packages
+        self.mileage = mileage
+        self.address = address
+        self.departure = departure
+
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s" % (self.speed, self.capacity,
+                                               self.load, self.packages, self.mileage, self.address, self.departure)
