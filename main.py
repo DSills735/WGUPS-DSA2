57c8bdb3eaadf2691ef7b5e6c88bfa1c4da60b60
DSills735 <David.sills100@gmail.com>
2025-05-01 23:44:32 -0500

    started CSV imports, added some python interpreter stuff for my machine

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+# David Sills, Student ID Number: 011355522, WGU
+
+import csv
+import datetime
+import packages
+import truck
+import hash_table
+
+with open("packages.csv") as csvfile:
+    package_csv = csv.reader(csvfile, delimiter=',')
+    package_csv = list(package_csv)
+
+with open("distances.csv") as csvfile:
+    distance_csv = csv.reader(csvfile, delimiter=',')
+    distance_csv = list(distance_csv)
+
 
 class main:
+
+
 
 
 
